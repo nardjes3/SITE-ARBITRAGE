f98b2888eebab4fcaf9098b862e52220b5c426fd
nardjes3 <saranardjes.khiat@univ-tlemcen.dz>
2023-04-02 11:33:46 +0200

    nona

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -1,1 +1,415 @@
-<h1>nn</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- PAGE TITLE HERE -->
+	<title>Admin</title>
+	<!-- Link Swiper's CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
+
+    <!-- Fontawesome CDN Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>
+	<!-- <link href="vendor/jquery-nice-select/css/nice-select.css" rel="stylesheet">
+	<link rel="stylesheet" href="vendor/nouislider/nouislider.min.css"> -->
+	<!-- Style css -->
+    <link href="css/AdminCss/dashboard.css" rel="stylesheet">
+	<link href="css/AdminCss/hexagone.css" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
+
+</head>
+<body>
+    <video autoplay loop muted  class="back-vid">
+	    <source src="images/imagesAdmin/Backg2.mp4" type="video/mp4"><source>
+	</video>
+	<div class="anim">
+	<svg viewBox="400 300 1350 1000">
+		<defs>
+			<linearGradient id="gradient" y1="0" y2="1">
+			 <stop stop-color="yellow" offset="0"/>
+			 <stop stop-color="green" offset="1"/>
+			</linearGradient>
+		   </defs>
+		<text x="70%" y="60%" stroke="url(#gradient)" fill="#232d2d">
+			FOOTBALL IS :
+		</text>
+	</svg>
+</div>
+	<div class="cards-container">
+		
+    <!--*******************
+        Preloader start
+    ********************-->
+    <div id="preloader">
+        <div class="waviy">
+		   <span style="--i:1">L</span>
+		   <span style="--i:2">o</span>
+		   <span style="--i:3">a</span>
+		   <span style="--i:4">d</span>
+		   <span style="--i:5">i</span>
+		   <span style="--i:6">n</span>
+		   <span style="--i:7">g</span>
+		   <span style="--i:8">.</span>
+		   <span style="--i:9">.</span>
+		   <span style="--i:10">.</span>
+		</div>
+    </div>
+    <!--*******************
+        Preloader end
+    ********************-->
+	<!-- <div class="slider">
+		<div class="container">
+			<div class="left">
+			  <div class="calendar">
+				<div class="month">
+				  <i class="fas fa-angle-left prev"></i>
+				  <div class="date">december 2015</div>
+				  <i class="fas fa-angle-right next"></i>
+				</div>
+				<div class="weekdays">
+				  <div>Sun</div>
+				  <div>Mon</div>
+				  <div>Tue</div>
+				  <div>Wed</div>
+				  <div>Thu</div>
+				  <div>Fri</div>
+				  <div>Sat</div>
+				</div>
+				<div class="days"></div>
+				<div class="goto-today">
+				  <div class="goto">
+					<input type="text" placeholder="mm/yyyy" class="date-input" />
+					<button class="goto-btn">Go</button>
+				  </div>
+				  <button class="today-btn">Today</button>
+				</div>
+			  </div>
+			</div>
+			<div class="right">
+			  <div class="today-date">
+				<div class="event-day">wed</div>
+				<div class="event-date">12th december 2022</div>
+			  </div>
+			  <div class="events"></div>
+			  <div class="add-event-wrapper">
+				<div class="add-event-header">
+				  <div class="title">Add Event</div>
+				  <i class="fas fa-times close"></i>
+				</div>
+				<div class="add-event-body">
+				  <div class="add-event-input">
+					<input type="text" placeholder="Event Name" class="event-name" />
+				  </div>
+				  <div class="add-event-input">
+					<input
+					  type="text"
+					  placeholder="Event Time From"
+					  class="event-time-from"
+					/>
+				  </div>
+				  <div class="add-event-input">
+					<input
+					  type="text"
+					  placeholder="Event Time To"
+					  class="event-time-to"
+					/>
+				  </div>
+				</div>
+				<div class="add-event-footer">
+				  <button class="add-event-btn">Add Event</button>
+				</div>
+			  </div>
+			</div>
+			<button class="add-event">
+			  <i class="fas fa-plus"></i>
+			</button>
+		  </div>
+	  
+</div> -->
+
+	
+
+
+<div class="hexag">
+
+<ul class="honeycomb">
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="images/imagesAdmin/alg.jpg">
+        <div class="honeycomb-cell_title">SACRIFICE</div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="images/imagesAdmin/ss.jpg">
+        <div class="honeycomb-cell_title">LOYALTY</div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="images/imagesAdmin/f.jpg">
+        <div class="honeycomb-cell_title">LIFE</div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="images/imagesAdmin/gg.jpg">
+        <div class="honeycomb-cell_title">HARD WORK</div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="images/imagesAdmin/coupe.jpg">
+        <div class="honeycomb-cell_title">RESPECT<small>for authority</small></div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="images/imagesAdmin/Goa.jpg">
+        <div class="honeycomb-cell_title">TEAM<small>game</small></div>
+    </li>
+    <li class="honeycomb-cell">
+        <img class="honeycomb-cell_img" src="images/imagesAdmin/foot.jpg">
+        <div class="honeycomb-cell_title">BELIEVING</div>
+    </li>
+    <li class="honeycomb-cell honeycomb_Hidden">
+    </li>
+</ul>
+
+</div>
+
+	<!-- <div class="slider">
+		<div class="cards-wrapper" >
+			
+				<ul class="pro_list">
+				  <li class="selected" style="background-image: url('images/imagesAdmin/etchiali.jpg');"  data-title="ETCHIALI abdelhak" data-description=".............."><h5 align="right">hi</h5></li>
+				  <li style="background-image: url('images/imagesAdmin/ghorbel.jpg');" data-title="GHORBEL mustapha" data-description=".........."></li>
+				  <li style="background-image: url('images/imagesAdmin/sampaio.jpg');" data-title="SAMPAIO Wilton" data-description="..........."></li>
+				  <li style="background-image: url('images/imagesAdmin/OIP.jpg');" data-title="TAYLOR Anthony " data-description="............."></li>
+				  <li style="background-image: url('images/imagesAdmin/ar05.jpg');" data-title=" URT Stuart" data-description="..........."></li>
+				  <li style="background-image: url('images/imagesAdmin/ar6.jpg');" data-title="ORTEGA Kevin" data-description="..........."></li>
+				</ul>	
+		
+				<section>
+				  <article class="card">
+					<div class="card_inner">
+					  <div class="card-header">
+						  <div class="pro_pic" style="background-image: url('images/imagesAdmin/etchiali.jpg');"></div>
+						  <h1 class="pro_title">Etchiali Abdelhak</h1>
+					  </div>
+					  <div class="pro_description">
+						  <p>........... </p>
+					  </div>
+					</div>
+				  </article>
+				</section>
+		
+			</div>
+			<div class="bg-radius">
+			  <span id="radius1"></span>
+			  <span id="radius2"></span>
+			  <span id="radius3"></span>
+			  <span id="radius4"></span>
+			  <span id="radius5"></span>
+			  <span id="radius6"></span>
+			</div>
+		</div> -->
+	</div>
+    <!--**********************************
+        Main wrapper start
+    ***********************************-->
+    <div id="main-wrapper">
+
+        <!--**********************************
+            Nav header start
+        ***********************************-->
+        <div class="nav-header">
+            <a href="index.html" class="brand-logo">
+				<img src="images/imagesAdmin/logo04.png" width="75" alt=""/>
+				<!-- <svg class="logo-abbr" width="20" height="20" > -->
+					<!-- <path class="svg-logo-primary-path" d="M48.3418 41.8457H41.0957C36.8148 41.8457 33.332 38.3629 33.332 34.082C33.332 29.8011 36.8148 26.3184 41.0957 26.3184H48.3418V19.2275C48.3418 16.9408 46.4879 15.0869 44.2012 15.0869H4.14062C1.85386 15.0869 0 16.9408 0 19.2275V48.8594C0 51.1462 1.85386 53 4.14062 53H44.2012C46.4879 53 48.3418 51.1462 48.3418 48.8594V41.8457Z" fill="#5BCFC5"/>
+					<path class="svg-logo-primary-path" d="M51.4473 29.4238H41.0957C38.5272 29.4238 36.4375 31.5135 36.4375 34.082C36.4375 36.6506 38.5272 38.7402 41.0957 38.7402H51.4473C52.3034 38.7402 53 38.0437 53 37.1875V30.9766C53 30.1204 52.3034 29.4238 51.4473 29.4238ZM41.0957 35.6348C40.2382 35.6348 39.543 34.9396 39.543 34.082C39.543 33.2245 40.2382 32.5293 41.0957 32.5293C41.9532 32.5293 42.6484 33.2245 42.6484 34.082C42.6484 34.9396 41.9532 35.6348 41.0957 35.6348Z" fill="#5BCFC5"/> -->
+				<!-- </svg> -->
+                
+				<p class="brand-title" width="118px" height="20px"  style="font-size: 19px; font-family:math ;">Whistle Experts </p> 
+				
+            </a>
+            <div class="nav-control">
+                <div class="hamburger">
+                    <span class="line"></span><span class="line"></span><span class="line"></span>
+                </div>
+            </div>
+        </div>
+        <!--**********************************
+            Nav header end
+        ***********************************-->
+		
+		
+		
+		<!--**********************************
+            Header start
+        ***********************************-->
+        <div class="header">
+            <div class="header-content">
+                <nav class="navbar navbar-expand">
+                    <div class="collapse navbar-collapse justify-content-between">
+                        <div class="header-left">
+							<!-- <div class="dashboard_bar">
+                                Dashboard 
+                            </div> -->
+                        </div>
+                        <ul class="navbar-nav header-right">
+							
+							<li class="nav-item dropdown notification_dropdown">
+                                <a class="nav-link" href="javascript:void(0);" data-bs-toggle="dropdown">
+									<img src="images/imagesAdmin/report.png" width="39" alt=" "/>
+									<span class="badge light text-white bg-primary rounded-circle">2</span>
+                                </a>
+								
+							</li>
+                            <li class="nav-item dropdown notification_dropdown">
+                                <a class="nav-link  ai-icon" href="javascript:void(0);" role="button" data-bs-toggle="dropdown">
+									<img src="images/imagesAdmin/inbox-mail.png" width="39" alt=" "/>
+                                    <!-- <span class="badge light text-white bg-primary rounded-circle">12</span> -->
+                                </a>
+                              
+                            </li>
+							<li class="nav-item dropdown notification_dropdown">
+                                <div class="action nav-link">
+                                    <div class="profile" onclick="menuToggle();">
+                                        <img src="images/imagesAdmin/user.png" alt="">
+                                    </div>
+                            
+                                    <div class="menu">
+                                        <h3>
+                                            User Account
+                                            <div>
+                                                Operational Team
+                                            </div>
+                                        </h3>
+                                        <ul>
+                                            <li>
+                                                <span class="material-icons icons-size">person</span>
+                                                <a href="profile.html">My Profile</a>
+                                            </li>
+                                           
+                                            
+                                            <li>
+                                                <span class="material-icons icons-size">account_balance_wallet</span>
+                                                <a href="#">Wallet</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <script>
+                                    function menuToggle(){
+                                        const toggleMenu = document.querySelector('.menu');
+                                        toggleMenu.classList.toggle('active')
+                                    }
+                                </script>
+                                
+							</li>
+                            
+                        </ul>
+                    </div>
+				</nav>
+			</div>
+		</div>
+        <!--**********************************
+            Header end ti-comment-alt
+        	***********************************-->
+
+
+										
+		<!-- -**********************************
+            Sidebar start
+        *********************************** -->
+        <div class="dlabnav">
+            <div class="dlabnav-scroll">
+				<ul class="metismenu" id="menu">
+					<li class="dropdown header-profile">
+						<a class="nav-link" href="javascript:void(0);" role="button" data-bs-toggle="dropdown">
+							<img src="images/imagesAdmin/bussiness-man.png" width="20" alt=""/>
+							<div class="header-info ms-3">
+								<span class="font-w600 ">Bonjour Monsieur</span>
+								<small class="text-end font-w400">Admin@gmail.com</small>
+							</div>
+						</a>
+						
+					</li>
+                    <li><a href="index.html" aria-expanded="false">
+						<img src="images/imagesAdmin/dashboard.png" width="30" alt=" "/>
+							<span class="nav-text">Dashboard</span>
+						</a>
+                    </li>
+                    <li><a href="list_arbi.html" aria-expanded="false">
+						<img src="images/imagesAdmin/liste (2).png" width="40" alt=" "/>
+							<span class="nav-text">liste arbitre</span>
+						</a>
+                    </li>
+                    <li><a href="calendar.html" aria-expanded="false">
+						<img src="images/imagesAdmin/calendar.png" width="37" alt=" "/>
+							<span class="nav-text">planning</span>
+						</a>
+                    </li>
+                    <!-- <li><a href="javascript:void()" aria-expanded="false">
+						<img src="images/imagesAdmin/checklist.png" width="37" alt=" "/>
+							<span class="nav-text">designation</span>
+						</a>
+                    </li> -->
+                    <li><a href="payement.html" aria-expanded="false">
+						<img src="images/imagesAdmin/paiement.png" width="37" alt=" "/>
+							<span class="nav-text">payment</span>
+						</a>
+                    </li>
+                </ul>
+			</div>
+        </div>
+        <!--**********************************
+            Sidebar end
+        ***********************************-->
+		
+		<!--**********************************
+            Content body start
+        ***********************************-->
+        <!-- <div class="content-body"> row -->
+			
+        </div> 
+        <!--**********************************
+            Content body end
+        ***********************************-->
+		
+		
+		
+        <!--**********************************
+            Footer start
+        ***********************************-->
+        <!-- <div class="footer">
+		
+            <div class="copyright">
+                <p>© Designed &amp; by <a href="#" target="_blank">Animation Coding</a> 2022</p>
+            </div>
+        </div> -->
+        <!--**********************************
+            Footer end
+        ***********************************-->
+
+		
+
+
+	</div>
+    <!--**********************************
+        Main wrapper end
+    ***********************************-->
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+	<script src="js/AdminJs/slide.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	
+    <script src="js/AdminJs/global.js"></script>
+	
+
+    <script src="js//AdminJs/custom.js"></script>
+	<script src="js//AdminJs/dlabnav.js"></script>
+
+	
+	
+</body>
+
+</html>
